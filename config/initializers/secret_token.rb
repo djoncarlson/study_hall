f9d5eecf2f2f1fdc4afaa2c1e09d3f7d495019e3
Devin <djoncarlson@gmail.com>
2011-03-15 11:22:07 -0500

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudyHall::Application.config.secret_token = '3d5d92636925da8ff7cd1a97496c18967bb232841a6336cc4691f52fb6a2da36a51027b6327ad0d8136a163e1fe22094bdb96a131904d495cd05b02fde2a9143'
