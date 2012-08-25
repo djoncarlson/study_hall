d6b36213f8e025c3ca046fce5b58d9726421f395
Devin Carlson <djoncarlson@gmail.com>
2012-08-25 09:53:22 -0500

    update notifier.rb

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -9,9 +9,9 @@ class Notifier < ActionMailer::Base
     @allassignment_array = Assignment.array_of_assignments(@attendance.all)
     @email_array = Assignment.array_of_emails(@missingassignment_array)  
 # production
-#    mail :to => "gfigueroa@cotopaxi.k12.ec, ewexler@cotopaxi.k12.ec, sbrazinsky@cotopaxi.k12.ec", :cc => @email_array, :subject => "Study Hall Attendance"
+    mail :to => "gfigueroa@cotopaxi.k12.ec, ewexler@cotopaxi.k12.ec, sbrazinsky@cotopaxi.k12.ec", :cc => @email_array, :subject => "Study Hall Attendance"
 # testing
-      mail :to => "djoncarlson@gmail.com", :subject => "Study Hall Attendance"
+#      mail :to => "djoncarlson@gmail.com", :subject => "Study Hall Attendance"
 
   end
   
@@ -21,12 +21,12 @@ class Notifier < ActionMailer::Base
     @old_assignment_emails = Assignment.array_of_emails(@old_assignment_array)
     unless @old_assignments.empty?
 # production      
-#      @old_assignments.each do |assn|
-#       mail :to => assn.user.email, :subject => "Old Study Hall Assignment"
-#      end
-#      mail :to => @old_assignment_emails, :subject => "Old Study Hall Assignments"
+      @old_assignments.each do |assn|
+       mail :to => assn.user.email, :subject => "Old Study Hall Assignment"
+      end
+      mail :to => @old_assignment_emails, :subject => "Old Study Hall Assignments"
 # testing
-      mail :to => "djoncarlson@gmail.com", :subject => "Old Study Hall Assignments"
+#      mail :to => "djoncarlson@gmail.com", :subject => "Old Study Hall Assignments"
     end
   end
   
